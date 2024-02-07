951889e78a6eab21aa01f6c478780075cf0ebe9b
FastLiu <109967434+FastLiu@users.noreply.github.com>
2024-02-07 15:28:53 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào em, anh có điều muốn nói. </br> tết này lì xì anh nhé!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "STK 0343261782 MB BANK nha",
+  buttonYes: "Ôge",
+  buttonNo: "Éo",
+  titleModar: "Cảm ơn bbi nhoa.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn Bbi nhaa anh bíc là em sẽ lì xì cho a mà. </br> Stk ở trên đó bbi. </br> bo thêm cho a cũng đcc </br> I Love you <3",
 };
